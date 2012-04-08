c11e7f3d2eb4e9e614406310611a1712fe3b504c
wentao.zhang <taowenzhang@gmail.com>
2012-04-08 11:19:34 +0800

    create basic data structur

--- models.py
+++ models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+	name = models.CharField(max_length=20)
+	description = models.CharField(max_length=100)
+	creation_time=models.DateTimeField(auto_now_add=True)
+	modification_time=models.DateTimeField(auto_now=True)	
+
+class Record(models.Model):
+	amount=models.FloatField()
+	detail=models.CharField(max_length=100)
+	occurrence_time=models.DataTime()
+	creation_time=models.DateTimeField(auto_now_add=True)
+	modification_time=models.DateTimeField(auto_now=True)	
+
+class Record_tag_maping(models.Model):
+	record_id = models.IntegerField()
+	tag_id = models.IntegerField()
