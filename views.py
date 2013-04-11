824eea07b148f9192c30a3929b6059776ccd3967
zhangwentao <taowenzhang@gmail.com>
2013-04-11 22:51:50 +0800

    给 accountbook 加入 登录验证

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,HttpResponseRedirect
 from django.template import RequestContext
 from models import Tag,Record,Record_tag_maping
 from datetime import datetime
@@ -11,9 +11,12 @@ def add_tag_page(request):
 	return render_to_response('add_tag.html',{},context_instance=RequestContext(request))
 
 def add_record_page(request):
-	title_tags = Tag.objects.filter(is_title=True)
-       	normal_tags = Tag.objects.filter(is_title = False) 			
-	return render_to_response('accountbook/add_record.html',{'title_tags':title_tags,'normal_tags':normal_tags},context_instance=RequestContext(request))
+	if request.user.is_authenticated():
+		title_tags = Tag.objects.filter(is_title=True)
+		normal_tags = Tag.objects.filter(is_title = False)
+		return render_to_response('accountbook/add_record.html',{'title_tags':title_tags,'normal_tags':normal_tags},context_instance=RequestContext(request))
+	else:
+		return HttpResponseRedirect('/admin')
 
 def add_tag(request):
 	var = request.POST
@@ -43,7 +46,11 @@ def add_record(request):
 	return HttpResponse('success');
 
 def show_record_page(request):
-	return render_to_response("accountbook/show_record.html")
+	if request.user.is_authenticated():
+		return render_to_response("accountbook/show_record.html")
+	else:
+		return HttpResponseRedirect('/admin')
+
 
 def record(request,year,month,day):
 	records = Record.objects.filter(occurrence_time = datetime(int(year),int(month),int(day)))
